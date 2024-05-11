432a3ef6bbe3f6a40965f7a55bc9aef723c4ebf0
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-05-11 16:35:48 -0300

    Operadores lógicos, unarios y de asignación

---
+++ Operadores logicos-unarios y de asignacion/operadores-logicos.js
@@ -0,0 +1,27 @@
+/* 
+OPERADORES LOGICOS:
+> (mayor)
+< (menor)
+>= (mayor igual)
+<= (menor igual)
+== (igual) se comparan los valores
+
+
+*/
+
+let Maria = 35;
+let Johana = 27;
+let Albana = 32;
+
+let edadMayor1 = Maria > Johana;
+document.write(edadMayor1);
+
+document.write("<br/>" + "------" + "<br/>");
+
+let edadMayor2 = Albana > Johana;
+document.write(edadMayor2);
+
+document.write("<br/>" + "------" + "<br/>");
+
+let edadMayor3 = Albana < Johana;
+document.write(edadMayor3);
