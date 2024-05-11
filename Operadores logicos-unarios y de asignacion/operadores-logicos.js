d9b6c5c98c037c0251c21fe637ad1058f884fd4e
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-05-11 16:59:51 -0300

    Operadores de lógicos,unarios y de asignación

--- Operadores logicos-unarios y de asignacion/operadores-logicos.js
+++ Operadores logicos-unarios y de asignacion/operadores-logicos.js
@@ -6,9 +6,20 @@ OPERADORES LOGICOS:
 <= (menor igual)
 == (igual) se comparan los valores
 
+OPERADORES UNARIOS
+++ incremento
+--decremento
+
+OPERADORES DE ASIGNACIÓN
++= 
+-=
+*=
+/=
+% (operador módulo)
 
 */
 
+// Operadores lógicos
 let Maria = 35;
 let Johana = 27;
 let Albana = 32;
@@ -25,3 +36,52 @@ document.write("<br/>" + "------" + "<br/>");
 
 let edadMayor3 = Albana < Johana;
 document.write(edadMayor3);
+
+document.write("<br/>" + "------" + "<br/>");
+
+// Operadores unarios
+Maria++;
+document.write(
+	`
+	 <p>el incremento de la edad es: ${Maria}</p>;
+	`
+
+	)
+
+
+Johana--;
+document.write(
+	`
+	 <p>el decremento de la edad es: ${Johana}</p>;
+	`
+
+	)
+
+document.write("<br/>" + "------" + "<br/>");
+
+//OPERADORES DE ASIGNACIÓN
+ let number = 20;
+ let number2 = 3;
+ let result = number % number2;
+ document.write("el resultado del módulo es:" + result);
+
+document.write("<br/>" + "------" + "<br/>");
+let incremento = 0;
+incremento += 10;
+document.write("incremento="+ incremento);
+
+document.write("<br/>" + "------" + "<br/>");
+
+let decremento = 5;
+decremento -= 3;
+document.write("decremento=" + decremento);
+
+document.write("<br/>" + "------" + "<br/>");
+
+number*= 5;
+document.write("incremento del producto=" + number);
+
+document.write("<br/>" + "------" + "<br/>");
+
+number/= 5;
+document.write("decremento de la división=" + number);
