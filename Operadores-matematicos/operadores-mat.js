3c8bf4852cf207d4b2a26f0397f2a032cc4e86ff
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-05-11 16:25:46 -0300

    Operadores matemáticos

---
+++ Operadores-matematicos/operadores-mat.js
@@ -0,0 +1,52 @@
+/* 
+OPERADORES MATEMÁTICOS:
+resta
+multiplicación
+división
+suma 
+
+*/
+
+let suma = 150 + 26;
+let resultadoSuma = `
+
+	<h2>el resultado de la suma es:
+	${suma}
+	<h2/>
+
+`;
+
+document.write(resultadoSuma);
+
+let resta = 150 - 26;
+let resultadoResta = `
+
+	<h2>el resultado de la resta es:
+	${resta}
+	<h2/>
+
+`;
+
+document.write(resultadoResta);
+
+let multi = (150 * 26);
+let resultadoMulti = `
+
+	<h2>el resultado de la multiplicación es:
+	${multi}
+	<h2/>
+
+`;
+
+document.write(resultadoMulti);
+
+let division = (150 / 26);
+let resultadoDivi = `
+
+	<h2>el resultado de la división es:
+	${division}
+	<h2/>
+
+`;
+
+document.write(resultadoDivi);
--- Variables/variables.js
+++ Variables/variables.js
@@ -12,5 +12,31 @@
 var numero1 = 10;
 var numero2 = 150;
 alert(numero1 + " " + numero2);
+document.write(typeof(numero1));
+document.write("<br/>");
 
 // string(cadenas)
+var nombre = "Gustavo";
+var apellidos = "Palacios Meyer";
+alert(nombre + " " + apellidos);
+document.write(typeof(nombre));
+document.write("<br/>");
+
+// boolean
+var bool = true;
+var bool2 = false;
+alert(bool + " - " + bool2);
+document.write(typeof(bool));
+document.write("<br/>");
+
+// undefined
+var entrada;
+alert(entrada);
+document.write(typeof(entrada));
+document.write("<br/>");
+
+var entrada2 = null;
+alert(entrada2);
+document.write(typeof(entrada2));
+document.write("<br/>");
+
